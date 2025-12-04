239efd6ef4e0097f8aa84a5d93cd6a23f2036bf6
Alexandros Kapsalidis <56820952+alexandroskapsalidis@users.noreply.github.com>
2025-12-04 19:12:49 +0200

    Remove unused stylesheet link
    Removed unused stylesheet link for rese.css.

--- add.php
+++ add.php
@@ -68,7 +68,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   <meta name="keywords" content="PHP, MySQL, cars, management, project">
   <link rel="icon" type="image/x-icon" href="car-favicon.png">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="rese.css" />
   <title>Autos Database</title>
 
   <style>
