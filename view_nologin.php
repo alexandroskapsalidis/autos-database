2f04a021c8e9bb405e05f4768b0f442f315442a5
Alexandros Kapsalidis <56820952+alexandroskapsalidis@users.noreply.github.com>
2025-12-04 19:16:14 +0200

    Remove 'rese.css' stylesheet link
    Removed the link to the 'rese.css' stylesheet.

--- view_nologin.php
+++ view_nologin.php
@@ -16,7 +16,6 @@ require_once "pdo.php";
   <meta name="keywords" content="PHP, MySQL, cars, management, project">
   <link rel="icon" type="image/x-icon" href="car-favicon.png">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="rese.css" />
   <title>Autos Database</title>
 
   <style>
