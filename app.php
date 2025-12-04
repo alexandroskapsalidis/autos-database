e3ecb429b3432f68d3f6e4de8bc3fd052cfc531e
Alexandros Kapsalidis <56820952+alexandroskapsalidis@users.noreply.github.com>
2025-12-04 19:13:32 +0200

    Remove unused stylesheet link
    Removed unused stylesheet link for rese.css.

--- app.php
+++ app.php
@@ -37,7 +37,6 @@ $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
   <meta name="keywords" content="PHP, MySQL, cars, management, project">
   <link rel="icon" type="image/x-icon" href="car-favicon.png">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="rese.css" />
   <title>Autos Database</title>
 
   <style>
